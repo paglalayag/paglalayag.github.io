5e2136cd62e591e9be9d5ccaacbacf3fec757d99
graial <al@paglalayag.net>
2025-04-18 18:23:19 +0800

    adjust importmap

--- importmap.rb
+++ importmap.rb
@@ -1,8 +1,8 @@
 pin 'application', to: 'assets/js/application.js', preload: true
 
-pin "@hotwired/turbo", to: "https://cdn.jsdelivr.net/npm/@hotwired/turbo@8.0.2/" # @8.0.2
+pin "@hotwired/turbo", to: "https://cdn.jsdelivr.net/npm/@hotwired/turbo@8.0.2/+esm" # @8.0.2
 pin "@hotwired/stimulus", to: "https://cdn.jsdelivr.net/npm/@hotwired/stimulus@3.2.2/+esm"
-pin "@hotwired/hotwire-native-bridge", to: "https://cdn.jsdelivr.net/npm/@hotwired/hotwire-native-bridge@1.0.0", preload: true
+pin "@hotwired/hotwire-native-bridge", to: "https://cdn.jsdelivr.net/npm/@hotwired/hotwire-native-bridge@1.0.0+esm", preload: true
 pin "@hotwired/stimulus-loading", to: "assets/js/stimulus-loading.js", preload: true
 
 pin_all_from "assets/js/controllers", to: "assets/js/controllers", under: "controllers"

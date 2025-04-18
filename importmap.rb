82a3fd90ddfd315e0cc1d6f1438070ecdcde9d35
graial <al@paglalayag.net>
2025-04-18 17:41:44 +0800

    adjust importmap

--- importmap.rb
+++ importmap.rb
@@ -1,4 +1,4 @@
-pin 'application', to: 'application.js', preload: true
+pin 'assets/js/application', to: 'assets/js/application.js', preload: true
 
 pin "@hotwired/turbo", to: "https://cdn.jsdelivr.net/npm/@hotwired/turbo@8.0.2/" # @8.0.2
 pin "@hotwired/stimulus", to: "https://cdn.jsdelivr.net/npm/@hotwired/stimulus@3.2.2/+esm"

080873db43b381102184b4124834976c0f9ca466
graial <al@paglalayag.net>
2025-05-13 01:44:52 +0800

    adjust favorite bridge toggle

--- assets/js/controllers/bridge/favorite_toggle_controller.js
+++ assets/js/controllers/bridge/favorite_toggle_controller.js
@@ -8,13 +8,14 @@ export default class FavoriteToggleController extends BridgeComponent {
 
 		const episode_url = this.bridgeElement.bridgeAttribute("episode_url")
 		this.send("connect", {episode_url}, () => {    
-			console.log("connected!")
-		    this.bridgeElement.click()
 		})
 	}
 
 	toggle() {
 		const episode_url = this.bridgeElement.bridgeAttribute("episode_url")
-		this.send("connect", {episode_url}, () => {})  
+		this.send("toggle", {episode_url}, () => {
+			console.log("toggle!")
+			this.bridgeElement.click()
+		})
 	}
 }

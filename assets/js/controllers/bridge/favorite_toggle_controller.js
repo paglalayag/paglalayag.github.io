8be9591f5b3890a3a5462e753c91a6fb71347210
graial <al@paglalayag.net>
2025-05-28 20:54:27 +0800

    add setFavorite handler

--- assets/js/controllers/bridge/favorite_toggle_controller.js
+++ assets/js/controllers/bridge/favorite_toggle_controller.js
@@ -3,20 +3,28 @@ import { BridgeComponent } from "@hotwired/hotwire-native-bridge"
 export default class FavoriteToggleController extends BridgeComponent {
 	static component = "favoriteToggle"
 
+	setFavorite(episode_url_from_connect) {
+		var episode_url
+
+		if (this.bridgeElement.bridgeAttribute("episode_url")) {
+			episode_url = this.bridgeElement.bridgeAttribute("episode_url")
+		} else {
+			episode_url = episode_url_from_connect
+		}
+			console.log("setFavorite!", this.jsonData)
+		// this.send("setFavorite", {episode_url}, () => {
+		// })
+	}
+	
 	connect() {
 		super.connect()
 
-		var episode_url
-		if (this.bridgeElement) {
-			episode_url = this.bridgeElement.bridgeAttribute("episode_url")
-		} else {
-			episode_url = "nil for now"
-		}
+		const episode_url = this.bridgeElement.bridgeAttribute("episode_url")
 		const episode_duration = this.bridgeElement.bridgeAttribute("episode_duration")
 		this.send("connect", {episode_url, episode_duration}, () => {
 		})
 
-		setFavorite() //the native-bridge will only respond to events it has already received a message from
+		setFavorite(episode_url) //the native-bridge will only respond to events it has already received a message from
 	}
 
 	toggle() {
@@ -26,12 +34,5 @@ export default class FavoriteToggleController extends BridgeComponent {
 		this.send("toggle", {episode_url}, () => {
 			this.bridgeElement.click()
 		})
-	}
-	setFavorite() {
-		const episode_url = this.bridgeElement.bridgeAttribute("episode_url")
-
-			console.log("setFavorite!", this.jsonData)
-		// this.send("setFavorite", {episode_url}, () => {
-		// })
 	}
 }

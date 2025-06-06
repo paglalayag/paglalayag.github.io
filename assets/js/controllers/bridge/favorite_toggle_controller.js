08b9cd4d6c14f05ea5af941b46ec6dd88ae3b6f3
graial <al@paglalayag.net>
2025-06-06 16:16:46 +0800

    adjust bridge reply data

--- assets/js/controllers/bridge/favorite_toggle_controller.js
+++ assets/js/controllers/bridge/favorite_toggle_controller.js
@@ -23,8 +23,15 @@ export default class FavoriteToggleController extends BridgeComponent {
 			episode_url = episode_url_from_connect
 		}
 		this.send("setFavorite", {episode_url}, message => {
-			console.log("setFavorite!", message.data.isFavorite)
+			console.log("setFavorite received! ", message.data.isFavorite)
+			const isFavorite = message.data.isFavorite
+			console.log("isFavorite is ", message.data.isFavorite)
 		})
+		this.send("display", { title, items }, message =>  {
+	      const selectedIndex = message.data.selectedIndex
+
+	      selectedItem.click()
+	    })
 	}
 
 	toggle() {
